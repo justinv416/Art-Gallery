7f56c53bc481d24caaf22e2619be460269b20f6b
Justin <vien_justin@hotmail.com>
2021-11-10 17:27:49 -0500

    second commit

--- app.js
+++ app.js
@@ -4,19 +4,29 @@ app.getId = function() {
     $.ajax({
         url: 'https://api.artic.edu/api/v1/artworks?&limit=100',
         dataType: 'json',
-    }).then(function(data) {
-        console.log(data.data)
-        
-        $('#img').attr('src', `https://www.artic.edu/iiif/2/${data.data[app.generateRandomNum(data.data.length)].image_id}/full/843,/0/default.jpg`)
+    }).then(function(object) {
+        console.log(object.data)
+        const randomNum = Math.floor(Math.random() * object.data.length)
+        app.displayArt(
+            object.data[randomNum].image_id, 
+            object.data[randomNum].artist_title,
+            object.data[randomNum].title,
+            object.data[randomNum].date_display,
+            object.data[randomNum].classification_title
+        )
     })
 }
 
 app.getId();
 
-app.generateRandomNum = function(max) {
-    return Math.floor(Math.random() * max)
+app.displayArt = function(data, artist, title, date, type) {
+    $('#img').attr('src', `https://www.artic.edu/iiif/2/${data}/full/843,/0/default.jpg`);
+    $('#artist-title').text(`${artist}`);
+    $('#art-title').text(`${title}`);
+    $('#date').text(`${date}`);
+    $('#classification').text(`${type}`);
 }
-    
+
 app.submitForm = function() {
     const form = $('form')
     form.on('submit', function(e){
@@ -27,4 +37,10 @@ app.submitForm = function() {
 
 app.submitForm()
 
-//on sumbit the api call will return an array with object ids, will need to apps these ids into another call 
+/*Issues:  
+
+generate random number not matching artist and title
+
+
+*/
+
