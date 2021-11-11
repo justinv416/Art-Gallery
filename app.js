9bc4aec21dc0775a3242954d830a9bde042cd47e
Justin <vien_justin@hotmail.com>
2021-11-11 16:59:26 -0500

    third commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const app = {};
 
-app.getId = function() {
+app.getArtworks = function() {
     $.ajax({
         url: 'https://api.artic.edu/api/v1/artworks?&limit=100',
         dataType: 'json',
@@ -12,35 +12,62 @@ app.getId = function() {
             object.data[randomNum].artist_title,
             object.data[randomNum].title,
             object.data[randomNum].date_display,
-            object.data[randomNum].classification_title
+            object.data[randomNum].classification_title,
+            object.data[randomNum].medium_display
         )
     })
 }
 
-app.getId();
+app.searchArtworks = function() {
+    $.ajax({
+        url: 'https://api.artic.edu/api/v1/artworks/search?',
+        dataType: 'json',
+        data: {
+            q: `${$('#input').val()}`
+        }
+    }).then(function(object) {
+        console.log(object.data)
+    })
+}
 
-app.displayArt = function(data, artist, title, date, type) {
+//Navigation toggle
+app.navControls = function() {
+    $('#nav-sub').hide();
+    $('#hamburger').on('click', function(){
+        $('#nav-sub').toggle('display');
+    });
+};
+
+app.init = function() {
+    app.getArtworks();
+    app.navControls();
+}
+
+app.init();
+
+app.displayArt = function(data, artist, title, date, type, medium) {
     $('#img').attr('src', `https://www.artic.edu/iiif/2/${data}/full/843,/0/default.jpg`);
     $('#artist-title').text(`${artist}`);
     $('#art-title').text(`${title}`);
     $('#date').text(`${date}`);
     $('#classification').text(`${type}`);
+    $('#medium').text(`${medium}`)
 }
 
 app.submitForm = function() {
     const form = $('form')
     form.on('submit', function(e){
         e.preventDefault();
-        app.getSearchData($('#input').val());
+        console.log($('#input').val())
+        app.searchArtworks()
     }); 
 }
 
-app.submitForm()
+app.submitForm();
+
+
 
 /*Issues:  
-
-generate random number not matching artist and title
-
 
 */
 
