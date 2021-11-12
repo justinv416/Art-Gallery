2ef600b4080caaf1e8f4635ac9f52c59b49a265d
Justin <vien_justin@hotmail.com>
2021-11-12 14:40:12 -0500

    fourth commit

--- app.js
+++ app.js
@@ -4,30 +4,41 @@ app.getArtworks = function() {
     $.ajax({
         url: 'https://api.artic.edu/api/v1/artworks?&limit=100',
         dataType: 'json',
-    }).then(function(object) {
-        console.log(object.data)
-        const randomNum = Math.floor(Math.random() * object.data.length)
+    }).then(function(response) {
+        console.log(response)
+        console.log(response.data)
+        const data = response.data;
+        const randomNum = app.generateRandomNum(data.length)
         app.displayArt(
-            object.data[randomNum].image_id, 
-            object.data[randomNum].artist_title,
-            object.data[randomNum].title,
-            object.data[randomNum].date_display,
-            object.data[randomNum].classification_title,
-            object.data[randomNum].medium_display
+            data[randomNum].image_id, 
+            data[randomNum].artist_title,
+            data[randomNum].title,
+            data[randomNum].date_display,
+            data[randomNum].classification_title,
+            data[randomNum].medium_display
         )
+        if(data[randomNum].image_id === null) {
+            console.log('image is not available')
+            $('#img').attr('src', './Icons/no-pictures.png');
+        }
     })
 }
 
-app.searchArtworks = function() {
-    $.ajax({
-        url: 'https://api.artic.edu/api/v1/artworks/search?',
-        dataType: 'json',
-        data: {
-            q: `${$('#input').val()}`
-        }
-    }).then(function(object) {
-        console.log(object.data)
-    })
+app.getArtworks();
+
+//random number generator
+app.generateRandomNum = function(max) {
+    return Math.floor(Math.random() * max)
+}
+
+//Displays art
+app.displayArt = function(imgSrc, title, artist, date, classification, medium) {
+    $('#img').attr('src', `https://www.artic.edu/iiif/2/${imgSrc}/full/843,/0/default.jpg`);
+    $('#art-title').text(`${title}`);
+    $('#artist-title').text(`${artist}`);
+    $('#date').text(`${date}`);
+    $('#classification').text(`${classification}`);
+    $('#medium').text(`${medium}`);
 }
 
 //Navigation toggle
@@ -45,25 +56,31 @@ app.init = function() {
 
 app.init();
 
-app.displayArt = function(data, artist, title, date, type, medium) {
-    $('#img').attr('src', `https://www.artic.edu/iiif/2/${data}/full/843,/0/default.jpg`);
-    $('#artist-title').text(`${artist}`);
-    $('#art-title').text(`${title}`);
-    $('#date').text(`${date}`);
-    $('#classification').text(`${type}`);
-    $('#medium').text(`${medium}`)
-}
+// app.searchArtworks = function() {
+//     $.ajax({
+//         url: 'https://api.artic.edu/api/v1/artworks/search?',
+//         dataType: 'json',
+//         data: {
+//             q: `${$('#input').val()}`
+//         }
+//     }).then(function(object) {
+//         console.log(object.data)
+//         console.log($(this))
+//         app.displayArtTest(object.data[1].thumbnail.lqip)
+//     })
+// }
 
-app.submitForm = function() {
-    const form = $('form')
-    form.on('submit', function(e){
-        e.preventDefault();
-        console.log($('#input').val())
-        app.searchArtworks()
-    }); 
-}
 
-app.submitForm();
+// app.submitForm = function() {
+//     const form = $('form')
+//     form.on('submit', function(e){
+//         e.preventDefault();
+//         console.log($('#input').val())
+//         app.searchArtworks()
+//     }); 
+// }
+
+// app.submitForm();
 
 
 
