e139bed8abe267fda3fcf4f25371bf77c18a522c
Justin <vien_justin@hotmail.com>
2021-11-17 13:27:11 -0500

    15th commit

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ app.generateRandomNum = function(max) {
     return Math.floor(Math.random() * max)
 }
 
-//Funciton that will display art with parameters that will be passed in during the ajax call.
-app.displayArt = function(imgSrc, title, artist, date, classification, medium, origin) {
+//Function that will display art with parameters that will be passed in during the ajax call.
+//Note: might consolidate this with displaySearchArt function in the future. 
+app.displayRandomArt = function(imgSrc, title, artist, date, classification, medium, origin) {
     $('.art__image').attr('src', `https://www.artic.edu/iiif/2/${imgSrc}/full/843,/0/default.jpg`);
     $('.art__description--title').text(`${title}`);
     $('.art__description--artist').text(`${artist}`);
@@ -17,17 +18,38 @@ app.displayArt = function(imgSrc, title, artist, date, classification, medium, o
     $('.art__description--origin').text(`${origin}`);
 };
 
+app.displaySearchArt = function(data) {
+    const dataHTML = `
+        <div id="results-container">
+            <img src=https://www.artic.edu/iiif/2/${data.image_id}/full/843,/0/default.jpg class="art__image--search"> 
+            <div class="art__description">
+                <div class="art__description--details">
+                    <h2 class="art__description--title">${data.title}</h2>
+                    <h2 class="art__description--artist">${data.artist_title}</h2>
+                    <h3 class="art__description--date">${data.date_display}</h3>
+                </div>
+                <div class="art__description--details">
+                    <h3 class="art__description--classification">${data.classification_title}</h3>
+                    <h3 class="art__description--medium">${data.medium_display}</h3>
+                    <h3 class="art__description--origin">${data.place_of_origin}</h3>
+                </div>
+            </div>
+        </div>
+    `;
+    $('#results').append(dataHTML);
+};
+
 //Function to make the ajax call to API and generate random artwork.
 app.getRandomArtworks = function() {
     //For performance reasons this API is limited to 100 pages
-    const randomNum = app.generateRandomNum(100)
+    const randomArtNum = app.generateRandomNum(100)
     $.ajax({
         url: `https://api.artic.edu/api/v1/artworks?`,
         dataType: 'json',
         method: 'GET',
         data: {
-            page: `${randomNum}`,
-            limit: '10'
+            page: `${randomArtNum}`,
+            limit: '50'
         }
     //Once promise is fufilled....    
     }).then(function(response) {
@@ -39,8 +61,8 @@ app.getRandomArtworks = function() {
         const data = response.data;
         //Store random number generator into a variable and pass the length of data array as a parameter.
         const randomNum = app.generateRandomNum(data.length)
-        //Call displayArt function and pass data from response as parameters.
-        app.displayArt(
+        //Call displayRandomArt function and pass data from response as parameters.
+        app.displayRandomArt(
             data[randomNum].image_id, 
             data[randomNum].title,
             data[randomNum].artist_title,
@@ -68,7 +90,8 @@ app.searchArtworks = function() {
         method: 'GET', 
         data: {
             q: `${$('#search-input').val()}`,
-            limit: '10'
+            //Limit currently set at 20 for performance reasons, will add pagination in the future
+            limit: '20'
         }
     }).then(function(response) {
         console.log(response.data)
@@ -85,25 +108,7 @@ app.searchArtworks = function() {
                 dataType: 'json'
             }).then(function(response){
                 const data = response.data;
-                const dataHTML = `
-                    <div id="results-container">
-                        <img src=https://www.artic.edu/iiif/2/${data.image_id}/full/843,/0/default.jpg class="art__image"> 
-                        <div class="art__description">
-                            <div class="art__description--details">
-                                <h2 class="art__description--title">${data.title}</h2>
-                                <h2 class="art__description--artist">${data.artist_title}</h2>
-                                <h3 class="art__description--date">${data.date_display}</h3>
-                            </div>
-                            <div class="art__description--details">
-                                <h3 class="art__description--classification">${data.classification_title}</h3>
-                                <h3 class="art__description--medium">${data.medium_display}</h3>
-                                <h3 class="art__description--origin">${data.place_of_origin}</h3>
-                            </div>
-                        </div>
-                    </div>
-                `
-                $('#results').append(dataHTML)
-                console.log(data)
+                app.displaySearchArt(data);
             });
         });
     });
@@ -112,33 +117,16 @@ app.searchArtworks = function() {
 //Function that will call the searchArtworks function on form submit.
 app.submitForm = function() {
     const form = $('form')
-    form.on('submit', function(e){
-        e.preventDefault();
+    form.on('submit', function(event){
+        event.preventDefault();
         console.log($('#search-input').val())
         $('#random-art').hide();
         app.searchArtworks();
     }); 
 };
 
-//Function that will return to main page. 
-app.returnToTitle = function() {
-    $('#title-main').on('click', function(){
-        $('#results').empty()
-        $('#random-art').hide()
-    });
-};
-
-//Shows search form on click.
-app.showSearch = function() {
-    $('#search-form').show();
-};
-
-//Function to initalize app
-app.init = function(){
-    $('#search-form').hide();
-    $('#random__art').hide();
-    app.submitForm();
-    app.returnToTitle();
+//Event listeners for buttons on page.
+app.btnControls = function() {
     $('#next-btn').on('click', function(){
         app.getRandomArtworks();
     });
@@ -147,7 +135,17 @@ app.init = function(){
         $('#results').empty()
         $('#random__art').show();
     });
-    $('#search-btn').on('click', app.showSearch)
+    $('#search-btn').on('click', function() {
+        $('#search-form').show();
+    });
+}
+
+//Function to initalize app
+app.init = function(){
+    $('#search-form').hide();
+    $('#random__art').hide();
+    app.submitForm();
+    app.btnControls()
 };
 
 //Initalize the app.
